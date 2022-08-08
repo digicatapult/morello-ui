0d44c58ed93703150295ed85042c48027829b8e4
Mark Addis <nitro_charlie@live.com>
2022-08-08 17:56:46 +0100

    fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 import { Container } from './Common'
 import dsbdLogo from '../assets/images/logo.png'
+
+export default function Header({ custom }) {
+  const logo = <img src={dsbdLogo} width={'120px'} height={'35px'} />
+
   return (
-    <Container,.  style  =  = {}
+    <Container
       height={'80px'}
       padding={'10px 20px'}
       justifyContent={'flex-start'}
@@ -11,6 +15,4 @@ import dsbdLogo from '../assets/images/logo.png'
       {custom || logo}
     </Container>
   )
-      {...style}
 }
- <≥≥ ≥≥...
