e240077f75e4fc0c969ac74b53f00cf653b357a0
n3op2 <paulius.michelevicius@gmail.com>
2022-08-08 15:07:07 +0100

    DSBD-ENV: sorting linting and cypress.

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,9 @@ const webpackConfig = require('./webpack.config')
 
 module.exports = defineConfig({
   e2e: {
+    webpackConfig,
+    framework: 'react',
+    bundler: 'webpack',
     supportFile: false,
     specPattern: 'cypress/integration/*.spec.js',
     baseUrl: 'http://localhost:3000',
