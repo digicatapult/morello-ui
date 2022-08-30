5b816bfa7a7f0b2a08d93bbb1f63f7b44c8c2a66
Matthew Dean <matthew.dean@digicatapult.org.uk>
2022-08-30 15:25:46 +0100

    Learn morello page

---
+++ src/components/Info.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import styled from 'styled-components'
+import { useNavigate } from 'react-router-dom'
+
+import Header from './Header'
+import HackerApp from './HackerApp'
+import { Context } from '../utils/context'
+import { Background } from './Background'
+import hackerIcon from '../assets/images/hacker-app-icon.png'
+import Box from './Box'
+import Popup, { PopBody, PopFooter, PopHeader } from './Popup'
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`
+
+const Content = styled.div`
+  display: flex;
+  background: #F0F0F0;
+  justify-content: center;
+  height 100%;
+  padding-top: 32px;
+  padding-left: 16px;
+  padding-right: 16px;
+`
+
+const ScreenshotPicture = styled.picture`
+  align-self: start;
+`
+
+const Screenshot = styled.img`
+  max-width: 100%;
+  `
+
+const QrCodeWrap = styled.a`
+  margin-left: -100px;
+  align-self: end;
+`
+
+const QrCode = styled.img`
+  max-width: 400px;
+`
+
+export default function Demo1(props) {
+  const nav = useNavigate()
+
+  return (
+    <Wrapper>
+      <Header {...props} showClose={true} />
+      <Content>
+        <ScreenshotPicture>
+          <source srcSet={`${props.imageName}.webp`} type="image/webp"/>
+          <source srcSet={`${props.imageName}.avif`} type="image/avif"/>
+          <Screenshot src={`${props.imageName}.png`} alt={`Image of website: ${props.link}`}/>
+        </ScreenshotPicture>
+        <QrCodeWrap href={props.link}>
+          <QrCode src={`${props.qrCode}.svg`} alt={`QR code link to ${props.link}`}/>
+        </QrCodeWrap>
+      </Content>
+    </Wrapper>
+  )
+}
