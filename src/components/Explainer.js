08285d1e4a621a68067f064aff370d660fc067a9
Jonathan Gray <jonmattgray@gmail.com>
2022-09-07 12:24:02 +0100

    fix import on merged component

--- src/components/Explainer.js
+++ src/components/Explainer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import Header from './Header'
+import Header from './shared/Header'
 
 const ExplainerWrapper = styled.div`
   width: min(
